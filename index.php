70825ba3405dc81376e4ea962a30d1a8f61ae2b1
miole2001 <blackspade2001@gmail.com>
2023-11-25 01:31:48 +0800

    [MIOLE]display the total number of booking

--- index.php
+++ index.php
@@ -49,6 +49,29 @@
                         </div>
                     </div>
                 </div>  
+                <!----------------- to diplay the total number of booking ---------------------------->
+
+                <div class="col-xl-5 col-md-6 mt-4">
+                    <div class="card bg-success text-white mb-4">
+                        <div class="card-body">
+                        <h3>Total Number of booking </h3>
+                            <?php
+                            require 'config/conn.php';
+
+                            $query = "SELECT id FROM booking ORDER BY id";
+                            $run_query = mysqli_query($conn, $query);
+
+                            $row = mysqli_num_rows($run_query);
+
+                            echo '<h1>' .$row. '</h1>'
+                            ?>
+                        </div>
+                        <div class="card-footer d-flex align-items-center justify-content-between">
+                            <a class="small text-white stretched-link" href="all-booking.php">View Details</a>
+                            <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                        </div>
+                    </div>
+                </div> 
             </div>
     </div>
 
